9d48b23532b0c7b628c78dec62edca1376d1cd41
Olesya Miller <olesyam.miller@gmail.com>
2019-12-23 04:17:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -39,7 +39,7 @@ class Owner
   def buy_cat(cat)
     #cat_to_buy = Cat.all.find { |kitty| kitty.name == cat }
     cat_to_buy.owner = self 
-    cat_to_buy = Cat.new(cat, cat_to_buy.owner )
+    cat_to_buy = Cat.new(cat, self )
   end
   
   # def list_pets
