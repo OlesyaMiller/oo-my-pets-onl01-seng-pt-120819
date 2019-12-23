a66e88029682fd2a09d44d9114a068645fce343c
Olesya Miller <olesyam.miller@gmail.com>
2019-12-23 02:08:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 class Dog
   attr_reader :name, :owner  
-  attr_writer :owner 
+  attr_writer :owner, :mood  
   
   def initialize(name, owner)
     @name = name 
     @owner = owner
+    
   end
 end
