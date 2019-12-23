45b36f7cb3a7253ea31a1f6ea439aff402e2b478
Olesya Miller <olesyam.miller@gmail.com>
2019-12-23 02:01:40 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
 
-  attr_accessor :owner 
+  attr_accessor :owner, :mood 
   attr_reader :name 
   def initialize(name, owner)
     @name = name 
     #owner = Owner.all.find {|owner| owner.cat == @name }
     @owner = owner
+    @mood = "nervous"
   end
 end
